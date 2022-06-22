d91f58291c6a46e4fbea8034e2ffc384768809a5
volkan <volkandogru11@gmail.com>
2022-06-22 09:29:47 +0300

    değişken tanımlamaya bakıldı

---
+++ 01_Js_Giris/app.js
@@ -0,0 +1,25 @@
+// js 1. gün giriş 
+
+//farklı veri türleri tanımlama
+
+let firstName = "volkan";
+let lastName = "doğru";
+let country = "Turkey";
+let city ="sakarya";
+let age=22;
+let isMarried =false;
+
+
+//değişkenleri sayı değerleriyle tanımlama
+
+const gravity = 9.81;
+const boilingPoint =100;
+const PI =3.14;
+
+
+//değişkenleri bir satırda virgül ile tanımlama
+
+let name ="volkan", job="software develooper",live="turkey";
+
+console.log(name);
+console.log(job);
