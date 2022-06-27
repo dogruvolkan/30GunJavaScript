67b58db6ef833d0b7b72227dc4c2688a69c7a859
volkan <volkandogru11@gmail.com>
2022-06-27 17:45:46 +0300

    json

---
+++ 16_Json/app.js
@@ -0,0 +1,21 @@
+//JSON
+
+//json example
+var user = {
+    "name": "John",
+    "age": 30,
+    "cars": [
+        "Ford",
+        "BMW",
+        "Fiat"
+    ]
+};
+
+
+//json string
+//json array
+//json object   olabilir
+
+
+//JSON.parse()      : json ı nesneye dönüştürmek için kullanılır
+//JSON.stringify()  : nesneyi json a dönüştürmek için kullanılır.
