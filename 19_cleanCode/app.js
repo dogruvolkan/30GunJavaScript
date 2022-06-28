e9c4be36c0f24901c4dfa47f8b086367445174e4
volkan <volkandogru11@gmail.com>
2022-06-28 17:18:56 +0300

    clean code

---
+++ 19_cleanCode/app.js
@@ -0,0 +1,20 @@
+//temiz kod nasıl yazılır
+
+//Airbnb javascript Style Guide
+//Javascript Standard Style Guide
+//Google javascript style Guide
+
+//değişken tanımlamasında var kullanılımasından kaçınmalıyız
+//değişken tanımlarken harf ile başlayığ camelCase yapısı kullanılabilir
+//çift tırnak yerine tek tırnak kulanımı daha popüler
+//normal fonksiyon yerine arrow fonksiyon kullanılabilir
+//değişken değer artışlarında ya da azalışlarında   +=  ya da -= tercih edilebilir
+//
+
+
+let firstName = 'volkan';
+const PI = 3.14;
+
+const fullName = (firstName , LastName) =>{
+    return `${firstName} ${LastName}`;
+}
